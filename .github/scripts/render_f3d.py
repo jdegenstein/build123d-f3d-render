6b18cd73e3bc66849ade425f28ab6471892cb7bb
jdegenstein <jdegenstein@users.noreply.github.com>
2025-12-09 11:21:05 -0600

    render_f3d.py -> use updated raytracing flags

--- .github/scripts/render_f3d.py
+++ .github/scripts/render_f3d.py
@@ -70,7 +70,8 @@ def render_with_f3d(file_path):
             "f3d", temp_step,
             f"--output={output_png}",
             "--resolution=1024,768",
-            "--samples=32",
+            "--raytracing",
+            "--raytracing-samples=32",
             "--up=+Z",
             "--verbose=quiet" 
         ]
